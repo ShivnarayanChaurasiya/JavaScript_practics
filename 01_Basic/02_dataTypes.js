93885d3352b213a57ba2856975b9be2f6c94898b
Shivnarayan Chaurasiya <amit902642@gmail.com>
2026-08-27 14:55:36 +0000

    next topic

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as new version 
+
+// alert (3+3) we are using nodejs , not browser 
+
+let name = "amit"
+let age = 20 
+let isLoggedIn = false 
+
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolen => true/false 
+// null => standalone value
+// undefined => 
+// Symbol => unique 
+
+
+
+console.log(typeof null);
+console.log(typeof undefined);  
---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+// console.log(typeof score);
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "333" => 33
+// "33abc" => NaN
+let isLoggedIn = "Amit";
+let boolenIsLoggedIn = Boolean( isLoggedIn)
+// console.log( boolenIsLoggedIn);
+
+let someNumber = 45;
+let stringNumber = String(someNumber);
+// console.log (stringNumber);
+// console.log(typeof stringNumber)
+
+
+
+
+// ************operations **********//
+
+let value = 3 
+let nagValue = -value
+console.log(negValue);
